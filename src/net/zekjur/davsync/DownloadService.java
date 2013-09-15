150f0ef1638fd2e550c2f02f75529acdb15dde06
Leonie <s0535631@htw-berlin.de>
2013-09-15 20:46:33 +0200

    hoffe es klappt jetzt...

--- src/net/zekjur/davsync/DownloadService.java
+++ src/net/zekjur/davsync/DownloadService.java
@@ -26,6 +26,7 @@ import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
@@ -33,6 +34,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,27 +50,7 @@ public class DownloadService extends IntentService {
 	String fileName;
 	
 	CountingInputStreamEntity entity = null;
-	
-	//public void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_share);
-       
-        /*Button b = (Button) findViewById(R.id.action_settings);
-        b.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                 showProgress(downloadFilePath);
-                     
-                    new Thread(new Runnable() {
-                        public void run() {
-                             downloadFile();
-                        }
-                      }).start();
-            }
-        }); */
-    	//Log.i("1. blabal", "ocreate");
-    //}
-     
+	 
 	public DownloadService() {
 		super("DownloadService");
 	}
@@ -84,14 +66,10 @@ public class DownloadService extends IntentService {
 	
 	protected void onHandleIntent(Intent intent) {
     	Log.i("2.", "onhandleintent");
-	    /*downloadFilePath = (String) intent.getExtras().get("url"); 
-	    fileName = (String) intent.getExtras().get("filename");
-		downloadFile();*/
 	}
 	
     private void downloadFile(){
     	Log.i("Yeah!", "DU BIST IN DOWNLOADFILE GELANDET!");
-
     	
     	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         Intent notificationIntent = new Intent(this, MainActivity.class);
@@ -116,16 +94,18 @@ public class DownloadService extends IntentService {
 		final NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		/* Setup Notification Manager End */
 		
-		Notification n = mBuilder.build();
+		final Notification n = mBuilder.build();
 
-        mNotificationManager.notify(100, n);
+						
+		mNotificationManager.notify(100, n);
 		
         mBuilder.setProgress(100, 50, false);
+		mNotificationManager.notify(100, n);
 
 		if (android.os.Build.VERSION.SDK_INT < 16)
-			mNotificationManager.notify(downloadFilePath, 0, mBuilder.getNotification());
+			mNotificationManager.notify(downloadFilePath, 100, mBuilder.getNotification());
 		else
-			mNotificationManager.notify(downloadFilePath, 0, mBuilder.build());
+			mNotificationManager.notify(downloadFilePath, 100, mBuilder.build());
     	
     	
         try {
@@ -150,27 +130,16 @@ public class DownloadService extends IntentService {
             InputStream is = ucon.getInputStream();
             BufferedInputStream bis = new BufferedInputStream(is);
             
-            
-			mBuilder.setProgress(100, 50, false);
-    		
-            
-            
-            
+            mBuilder.setProgress(100, 50, false);
+    		mNotificationManager.notify(100, n);
+                                   
             /* Read bytes to the Buffer until there is nothing more to read(-1). */
             ByteArrayBuffer baf = new ByteArrayBuffer(50);
             int current = 0;
             while ((current = bis.read()) != -1) {
                 downloadSize += current;
                 baf.append((byte) current);   
-                
-                // update the progressbar //
-                /*runOnUiThread(new Runnable() {
-                    public void run() {
-                        pb.setProgress(downloadSize);
-                        float per = ((float)downloadSize/totalSize) * 100;
-                        cur_val.setText("Downloaded " + downloadSize + "KB / " + totalSize + "KB (" + (int)per + "%)" );
-                    }
-                });*/
+                               
             }
             
             /* Convert the Bytes read to a String. */
@@ -181,46 +150,18 @@ public class DownloadService extends IntentService {
 
         } catch (IOException e) {
         	Log.d("Error", e.toString());
-        	showError("Error : Please check your oSEGHILshgl blabla " + e);
         }
            
         
-        
-
         // When the loop is finished, updates the notification
-        mBuilder.setContentText("Download complete")
+//        mBuilder.setContentText("Download complete")
         // Removes the progress bar
-                .setProgress(0, 0, false);
-        mNotificationManager.notify(1001, mBuilder.build());
+//                .setProgress(0, 0, false);
+        mNotificationManager.notify(100, mBuilder.build());
 		if (android.os.Build.VERSION.SDK_INT < 16)
 			mNotificationManager.notify(downloadFilePath, 0, mBuilder.getNotification());
 		else
 			mNotificationManager.notify(downloadFilePath, 0, mBuilder.build());
-    }
-     
-    private void showError(final String err){
-        /*runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(DownloadService.this, err, Toast.LENGTH_LONG).show();
-            }
-        });*/
-    }
-     
-    private void showProgress(String file_path){
-        //dialog = new Dialog(DownloadService.this);
-        /*dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.myprogressdialog);
-        dialog.setTitle("Download Progress");
- 
-        TextView text = (TextView) dialog.findViewById(R.id.tv1);
-        text.setText("Downloading file from ... " + file_path);
-        cur_val = (TextView) dialog.findViewById(R.id.cur_pg_tv);
-        cur_val.setText("Starting download...");
-        dialog.show();
-         
-        pb = (ProgressBar)dialog.findViewById(R.id.progress_bar);
-        pb.setProgress(0);*/
-        //pb.setProgressDrawable(getResources().getDrawable(R.drawable.green_progress)); 
     }
 }
 
